294942b764b39bb81e44551c202d797501754140
owenwuwenxi <owu@iu.edu>
2020-09-18 22:32:59 -0400

    Updating js
    Updated JS file

--- app/public/js/profile.app.js
+++ app/public/js/profile.app.js
@@ -1,12 +1,29 @@
 var app = new Vue ({
   el: '#usefprofile',
   data: {
+    userName: '',
+    userEmail:'',
+    userImageLarge:'',
+    userImageThumb:''
+  },
 
-  },
   created(){
-    console.log('this vue app has been created!');
+    this.fetchUser();
   },
+
   methods:{
-    
+    fetchUser: function(){
+      fetch('https://randomuser.me/api/')
+      .then(response => response.json())
+      .then(data => {
+        var userData = data.results[0];
+        console.log(userData);
+        this.userName = userData.name.first + " " + userData.name.last;
+        this.userEmail = userData.email;
+        this.userImageLarge = userData.picture.large;
+        this.userImageThumb = userData.picture.thumbnail;
+        console.log('user country' + userData.location.country);
+      });
+    }
   }
 })
